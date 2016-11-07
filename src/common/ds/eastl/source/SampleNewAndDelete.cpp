343767c103f59f0add63b225a8f19a573cce6ce6
kiddinglife <jakezhang1989@hotmail.com>
2016-11-07 17:30:34 +1100

    [fix] complie errors in eclispe for I added eastl yesterday

--- src/common/ds/eastl/source/SampleNewAndDelete.cpp
+++ src/common/ds/eastl/source/SampleNewAndDelete.cpp
@@ -1,51 +1,56 @@
 /////////////////////////////////////////////////////////////////////////////
 // Copyright (c) Electronic Arts Inc. All rights reserved.
 /////////////////////////////////////////////////////////////////////////////
-
-
 #include <EABase/eabase.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <malloc.h>
 #include <new>
-
 
 ///////////////////////////////////////////////////////////////////////////////
 // throw specification wrappers, which allow for portability.
 ///////////////////////////////////////////////////////////////////////////////
 
 #if defined(EA_COMPILER_NO_EXCEPTIONS) && (!defined(__MWERKS__) || defined(_MSL_NO_THROW_SPECS)) && !defined(EA_COMPILER_RVCT)
-	#define THROW_SPEC_0    // Throw 0 arguments
-	#define THROW_SPEC_1(x) // Throw 1 argument
+#define THROW_SPEC_0    // Throw 0 arguments
+#define THROW_SPEC_1(x) // Throw 1 argument
 #else
-	#define THROW_SPEC_0    throw()
-	#define THROW_SPEC_1(x) throw(x)
+#define THROW_SPEC_0    throw()
+#define THROW_SPEC_1(x) throw(x)
 #endif
 
 #ifdef _WIN32
 #pragma warning( disable : 4290 )
-#define malloc_aligned _aligned_offset_malloc
+#define aligned_alloc _aligned_offset_malloc
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
 // operator new used by EASTL
 ///////////////////////////////////////////////////////////////////////////////
 
-void* operator new[](size_t size, const char* /*name*/, int /*flags*/, 
-					 unsigned /*debugFlags*/, const char* /*file*/, int /*line*/) THROW_SPEC_1(std::bad_alloc)
+void* operator new[](size_t size, const char* /*name*/, int /*flags*/,
+		unsigned /*debugFlags*/, const char* /*file*/, int /*line*/)
+		THROW_SPEC_1(std::bad_alloc)
 {
 	return malloc(size);
 }
 
-void* operator new[](size_t size, size_t alignment, size_t alignmentOffset, const char* /*name*/, 
-					 int flags, unsigned /*debugFlags*/, const char* /*file*/, int /*line*/) THROW_SPEC_1(std::bad_alloc)
+void* operator new[](size_t size, size_t alignment, size_t alignmentOffset,
+		const char* /*name*/, int flags, unsigned /*debugFlags*/,
+		const char* /*file*/, int /*line*/) THROW_SPEC_1(std::bad_alloc)
 {
-	// Substitute your aligned malloc. 
-	return malloc_aligned(size, alignment, alignmentOffset);
+	// Substitute your aligned malloc.
+
+#ifdef _WIN32
+	return aligned_alloc(size, alignment, alignmentOffset);
+#else
+	return aligned_alloc(size, alignment);
+#endif
 }
-
-
 
 ///////////////////////////////////////////////////////////////////////////////
 // Other operator new as typically required by applications.
+// malloc() will align by default by 4 bytes
 ///////////////////////////////////////////////////////////////////////////////
 
 void* operator new(size_t size) THROW_SPEC_1(std::bad_alloc)
@@ -53,12 +58,10 @@ void* operator new(size_t size) THROW_SPEC_1(std::bad_alloc)
 	return malloc(size);
 }
 
-
 void* operator new[](size_t size) THROW_SPEC_1(std::bad_alloc)
 {
 	return malloc(size);
 }
-
 
 ///////////////////////////////////////////////////////////////////////////////
 // Operator delete, which is shared between operator new implementations.
@@ -66,19 +69,13 @@ void* operator new[](size_t size) THROW_SPEC_1(std::bad_alloc)
 
 void operator delete(void* p) THROW_SPEC_0
 {
-	if(p) // The standard specifies that 'delete NULL' is a valid operation.
+	if (p) // The standard specifies that 'delete NULL' is a valid operation.
 		free(p);
 }
-
 
 void operator delete[](void* p) THROW_SPEC_0
 {
-	if(p)
+	if (p)
 		free(p);
 }
-
-
-
-
-
 
--- unittest/test-debugging.cc
+++ unittest/test-debugging.cc
@@ -20,125 +20,121 @@ DECLARE_DEBUG_COMPONENT2("UNIT-TEST", 0);
 
 TEST(GECO_DEBUGGING_MSGLOG, test_msg_macros)
 {
-	Leave("test_msg_macros", "test_msg_macros");
-	Enter("test_msg_macros", "test_msg_macros");
-	VERBOSE_MSG("VERBOSE_MSG\n");
-	DEBUG_MSG("DEBUG_MSG %d\n", 12);
-	INFO_MSG("INFO_MSG %s\n", "hello world");
-	NOTICE_MSG("NOTICE_MSG %s\n", "hello world");
-	ERROR_MSG("ERROR_MSG %s\n", "hello world");
+    Leave("test_msg_macros", "test_msg_macros");
+    Enter("test_msg_macros", "test_msg_macros");
+    VERBOSE_MSG("VERBOSE_MSG\n");
+    DEBUG_MSG("DEBUG_MSG %d\n", 12);
+    INFO_MSG("INFO_MSG %s\n", "hello world");
+    NOTICE_MSG("NOTICE_MSG %s\n", "hello world");
+    ERROR_MSG("ERROR_MSG %s\n", "hello world");
 }
 
 static bool critical_cb_is_called = false;
-static bool criticalcallback(int component_priority, int msg_priority,
-		const char * format, va_list args_list)
+static bool criticalcallback(int component_priority, int msg_priority, const char * format, va_list args_list)
 {
-	// !!! never call debugging macro in debugging cb will loop DEBUG_MSG("DEBUG_MSG %d\n", 12);
-	printf("criticalcallback msg_priority=%d!\n", msg_priority);
-	critical_cb_is_called = true;
-	//return true, no default printout operation, which is print to stdout at this moment
-	//return true, default printout operation, which is print to stdout at this moment
-	return false;
+    // !!! never call debugging macro in debugging cb will loop DEBUG_MSG("DEBUG_MSG %d\n", 12);
+    printf("criticalcallback msg_priority=%d!\n", msg_priority);
+    critical_cb_is_called = true;
+    //return true, no default printout operation, which is print to stdout at this moment
+    //return true, default printout operation, which is print to stdout at this moment
+    return false;
 }
 TEST(GECO_DEBUGGING_MSGLOG, test_critical_msg)
 {
-	{
-		scoped_stack_tracker_t t("dummy call", "test-debugging file", 123);
-	}
+    {
+        scoped_stack_tracker_t t("dummy call", "test-debugging file", 123);
+    }
 
-	critical_msg_cb_t dcb = std::bind(criticalcallback, std::placeholders::_1,
-			std::placeholders::_2, std::placeholders::_3,
-			std::placeholders::_4);
-	log_msg_filter_t::get_instance().add_critical_cb(&dcb);
-	CRITICAL_MSG("THIS IS CRITICAL_MSG\n");
+    critical_msg_cb_t dcb = std::bind(criticalcallback, std::placeholders::_1, std::placeholders::_2,
+            std::placeholders::_3, std::placeholders::_4);
+    log_msg_filter_t::get_instance().add_critical_cb(&dcb);
+    CRITICAL_MSG("THIS IS CRITICAL_MSG\n");
 }
 
 static bool debug_cb_is_called = false;
-static bool debugcallback(int component_priority, int msg_priority,
-		const char * format, va_list args_list)
+static bool debugcallback(int component_priority, int msg_priority, const char * format, va_list args_list)
 {
-	// !!! never call debugging macro in debugging cb will loop DEBUG_MSG("DEBUG_MSG %d\n", 12);
-	printf("debugcallback msg_priority=%d!\n", msg_priority);
-	debug_cb_is_called = true;
-	//return true, no default printout operation, which is print to stdout at this moment
-	//return true, default printout operation, which is print to stdout at this moment
-	return false;
+    // !!! never call debugging macro in debugging cb will loop DEBUG_MSG("DEBUG_MSG %d\n", 12);
+    printf("debugcallback msg_priority=%d!\n", msg_priority);
+    debug_cb_is_called = true;
+    //return true, no default printout operation, which is print to stdout at this moment
+    //return true, default printout operation, which is print to stdout at this moment
+    return false;
 }
 TEST(GECO_DEBUGGING_MSGLOG, test_msg_cb)
 {
-	debug_msg_cb_t dcb = std::bind(debugcallback, std::placeholders::_1,
-			std::placeholders::_2, std::placeholders::_3,
-			std::placeholders::_4);
-	log_msg_filter_t::get_instance().add_debug_cb(&dcb);
-	VERBOSE_MSG("VERBOSE_MSG\n");
-	ASSERT_EQ(debug_cb_is_called, true);
+    debug_msg_cb_t dcb = std::bind(debugcallback, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3,
+            std::placeholders::_4);
+    log_msg_filter_t::get_instance().add_debug_cb(&dcb);
+    VERBOSE_MSG("VERBOSE_MSG\n");
+    ASSERT_EQ(debug_cb_is_called, true);
 }
 
 TEST(GECO_DEBUGGING_MSGLOG, geco_asset_dev_cause_real_assertion_when_has_dev_assert_is_false)
 {
-	// Runtime disable fatal assertions
-	geco::debugging::log_msg_filter_t::get_instance().has_dev_assert_ = false;
-	// when has_dev_assert_ = false, GECO_ASSERT will not cause a REAL assert
-	GECO_ASSERT_DEV(false);
+    // Runtime disable fatal assertions
+    geco::debugging::log_msg_filter_t::get_instance().has_dev_assert_ = false;
+    // when has_dev_assert_ = false, GECO_ASSERT will not cause a REAL assert
+    GECO_ASSERT_DEV(false);
 }
 TEST(GECO_DEBUGGING_MSGLOG, geco_asset_dev_cause_real_assertion_when_has_dev_assert_is_true)
 {
-	// Runtime enable dev fatal assertions
-	geco::debugging::log_msg_filter_t::get_instance().has_dev_assert_ = true;
-	// when has_dev_assert_ = true (default), GECO_ASSERT will cause a REAL assert like GECO_ASSERT_DEV
-	GECO_ASSERT_DEV(false);
+    // Runtime enable dev fatal assertions
+    geco::debugging::log_msg_filter_t::get_instance().has_dev_assert_ = true;
+    // when has_dev_assert_ = true (default), GECO_ASSERT will cause a REAL assert like GECO_ASSERT_DEV
+    GECO_ASSERT_DEV(false);
 }
 
 TEST(GECO_DEBUGGING_MSGLOG, geco_asset_dev_if_not_does_block_codes_will_not_run_when_has_dev_assert_is_true_and_assert_value_false)
 {
-	// when has_dev_assert_ = true (default), 
-	// GECO_ASSERT_DEV_IFNOT will cause a REAL assert like GECO_ASSERT_DEV
-	bool runblockcodes = false;
-	log_msg_filter_t::get_instance().has_dev_assert_ = true;
-	GECO_ASSERT_DEV_IFNOT(false)
-	{
-		runblockcodes = true;
-		INFO_MSG("you should not see me as has_dev_assert_ == true.\n\n");
-	}
-	EXPECT_FALSE(runblockcodes);
+    // when has_dev_assert_ = true (default),
+    // GECO_ASSERT_DEV_IFNOT will cause a REAL assert like GECO_ASSERT_DEV
+    bool runblockcodes = false;
+    log_msg_filter_t::get_instance().has_dev_assert_ = true;
+    GECO_ASSERT_DEV_IFNOT(false)
+    {
+        runblockcodes = true;
+        INFO_MSG("you should not see me as has_dev_assert_ == true.\n\n");
+    }
+    EXPECT_FALSE(runblockcodes);
 }
 TEST(GECO_DEBUGGING_MSGLOG, geco_asset_dev_if_not_does_block_codes_will_not_run_when_has_dev_assert_is_true_and_assert_value_true)
 {
-	// when has_dev_assert_ = true (default), 
-	// GECO_ASSERT_DEV_IFNOT will cause a REAL assert like GECO_ASSERT_DEV
-	bool runblockcodes = false;
-	log_msg_filter_t::get_instance().has_dev_assert_ = true;
-	GECO_ASSERT_DEV_IFNOT(true)
-	{
-		runblockcodes = true;
-		INFO_MSG("you should not see me as it is true.\n");
-	}
-	EXPECT_FALSE(runblockcodes);
+    // when has_dev_assert_ = true (default),
+    // GECO_ASSERT_DEV_IFNOT will cause a REAL assert like GECO_ASSERT_DEV
+    bool runblockcodes = false;
+    log_msg_filter_t::get_instance().has_dev_assert_ = true;
+    GECO_ASSERT_DEV_IFNOT(true)
+    {
+        runblockcodes = true;
+        INFO_MSG("you should not see me as it is true.\n");
+    }
+    EXPECT_FALSE(runblockcodes);
 }
 TEST(GECO_DEBUGGING_MSGLOG, geco_asset_dev_if_not_does_block_codes_will_run_when_has_dev_assert_is_false_and_assert_value_false)
 {
-	// when has_dev_assert_ = false (default), 
-	// GECO_ASSERT_DEV_IFNOT will cause a REAL assert like GECO_ASSERT_DEV
-	bool runblockcodes = false;
-	log_msg_filter_t::get_instance().has_dev_assert_ = false;
-	GECO_ASSERT_DEV_IFNOT(false)
-	{
-		runblockcodes = true;
-		INFO_MSG("you should see me as has_dev_assert_ == false and asserts false.\n\n");
-	}
-	EXPECT_TRUE(runblockcodes);
+    // when has_dev_assert_ = false (default),
+    // GECO_ASSERT_DEV_IFNOT will cause a REAL assert like GECO_ASSERT_DEV
+    bool runblockcodes = false;
+    log_msg_filter_t::get_instance().has_dev_assert_ = false;
+    GECO_ASSERT_DEV_IFNOT(false)
+    {
+        runblockcodes = true;
+        INFO_MSG("you should see me as has_dev_assert_ == false and asserts false.\n\n");
+    }
+    EXPECT_TRUE(runblockcodes);
 }
 TEST(GECO_DEBUGGING_MSGLOG, geco_asset_dev_if_not_does_block_codes_will_not_run_when_has_dev_assert_is_false_and_assert_value_true)
 {
-	// when has_dev_assert_ = true (default), 
-	// GECO_ASSERT_DEV_IFNOT will cause a REAL assert like GECO_ASSERT_DEV
-	bool runblockcodes = false;
-	log_msg_filter_t::get_instance().has_dev_assert_ = false;
-	GECO_ASSERT_DEV_IFNOT(true)
-	{
-		runblockcodes = true;
-		INFO_MSG("you should not see me as it is true.\n");
-	}
-	EXPECT_FALSE(runblockcodes);
+    // when has_dev_assert_ = true (default),
+    // GECO_ASSERT_DEV_IFNOT will cause a REAL assert like GECO_ASSERT_DEV
+    bool runblockcodes = false;
+    log_msg_filter_t::get_instance().has_dev_assert_ = false;
+    GECO_ASSERT_DEV_IFNOT(true)
+    {
+        runblockcodes = true;
+        INFO_MSG("you should not see me as it is true.\n");
+    }
+    EXPECT_FALSE(runblockcodes);
 }
 
