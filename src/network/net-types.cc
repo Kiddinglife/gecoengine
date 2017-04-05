bf2697584dd77ed4c4e3d23fdad43327dc293fbe
Jake Zhang <jakezhang1989@hotmail.com>
2017-04-05 21:56:39 +1000

    working on networkelement

--- src/network/net-types.cc
+++ src/network/net-types.cc
@@ -11,10 +11,10 @@
 #include <arpa/inet.h>
 #endif
 
+/*GecoNetAddress*/
 char GecoNetAddress::ms_pcStringBuf[2][GecoNetAddress::MAX_STRLEN];
 int GecoNetAddress::ms_iCurrStringBuf = 0;
 const GecoNetAddress GecoNetAddress::NONE(0, 0);
-
 bool WatcherStringToValue(const char * valueStr, GecoNetAddress & value)
 {
 	int a1, a2, a3, a4, a5;
@@ -30,7 +30,6 @@ bool WatcherStringToValue(const char * valueStr, GecoNetAddress & value)
 	value.m_uiIP = ntohl(value.m_uiIP);
 	return true;
 }
-
 GecoNetAddress::GecoNetAddress(char* str)
 	:m_uiIP(0)
 	, m_uiPort(0)
@@ -38,7 +37,6 @@ GecoNetAddress::GecoNetAddress(char* str)
 {
 	SetFromString(str);
 }
-
 int GecoNetAddress::WriteToString(char * str, int length) const
 {
 	uint	hip = ntohl(m_uiIP);
@@ -52,14 +50,12 @@ int GecoNetAddress::WriteToString(char * str, int length) const
 		(int)(unsigned char)(hip),
 		(int)hport);
 }
-
 char * GecoNetAddress::c_str() const
 {
 	char * buf = GecoNetAddress::NextStringBuf();
 	this->WriteToString(buf, MAX_STRLEN);
 	return buf;
 }
-
 const char * GecoNetAddress::IPAsString() const
 {
 	uint	hip = ntohl(m_uiIP);
@@ -74,7 +70,6 @@ const char * GecoNetAddress::IPAsString() const
 
 	return buf;
 }
-
 bool GecoNetAddress::SetFromString(char* str)
 {
 	if (!str)
@@ -94,26 +89,21 @@ bool GecoNetAddress::SetFromString(char* str)
 
 	return true;
 }
-
-#if FV_ENABLE_WATCHERS
-GecoWatcher & GecoNetAddress::GetWatcher()
+// @TODO
+GecoWatcher* GecoNetAddress::GetWatcher()
 {
-	static GecoMemberWatcher<char *, GecoNetAddress>	* watchMe = NULL;
-
-	if (watchMe == NULL)
-	{
-		watchMe = new GecoMemberWatcher<char *, GecoNetAddress>(
-			*((GecoNetAddress*)NULL),
-			&GecoNetAddress::operator char*,
-			static_cast<void (GecoNetAddress::*)(char*)>(NULL)
-			);
-	}
-
-	return *watchMe;
+	//static GecoMemberWatcher<char *, GecoNetAddress>	* watchMe = NULL;
+	//if (watchMe == NULL)
+	//{
+	//	watchMe = new GecoMemberWatcher<char *, GecoNetAddress>(
+	//		*((GecoNetAddress*)NULL),
+	//		&GecoNetAddress::operator char*,
+	//		static_cast<void (GecoNetAddress::*)(char*)>(NULL)
+	//		);
+	//}
+	//return *watchMe;
+	return NULL;
 }
-#endif // FV_ENABLE_WATCHERS
-
-
 char * GecoNetAddress::NextStringBuf()
 {
 	ms_iCurrStringBuf = (ms_iCurrStringBuf + 1) % 2;
@@ -121,3 +111,96 @@ char * GecoNetAddress::NextStringBuf()
 }
 
 
+
+/*GecoNetInterfaceElement*/
+class BundleDataPos
+{
+private:
+	GecoNetPacket * pPacket_;
+	uchar * pCurr_;
+public:
+	BundleDataPos(GecoNetPacket * pPacket, void * pCurr) :
+		pPacket_(pPacket),
+		pCurr_((uchar*)pCurr)
+	{
+		assert(pPacket->Body() <= pCurr && pCurr < pPacket->Back());
+	}
+	GecoNetPacket * pPacket() const { return pPacket_; }
+	uchar * pData() const { return pCurr_; }
+	bool Advance(unsigned int distance)
+	{
+		if ((pPacket_ == NULL) || (pCurr_ == NULL))
+			return false;
+
+		int remainingLength = distance;
+		uchar * pEnd = (uchar*)pPacket_->Back();
+
+		while (remainingLength >= (pEnd - pCurr_))
+		{
+			remainingLength -= (pEnd - pCurr_);
+			pPacket_ = pPacket_->Next();
+			if (pPacket_ == NULL)
+			{
+				g_network_logger->error("BundleDataPos::AdvancedBy: Ran out of packets.\n");
+				return false;
+			}
+			pCurr_ = (uchar*)pPacket_->Body();
+			pEnd = (uchar*)pPacket_->Back();
+		}
+		pCurr_ += remainingLength;
+		return true;
+	}
+};
+const GecoNetInterfaceElement GecoNetInterfaceElement::REPLY("Reply", REPLY_MESSAGE_IDENTIFIER, VARIABLE_LENGTH_MESSAGE, 4);
+const float GecoNetInterfaceElementWithStats::AVERAGE_BIAS = -2.f / (5 + 1);
+int GecoNetInterfaceElement::HeaderSize() const
+{
+	int headerLen = sizeof(GecoNetMessageID);
+	switch (m_uiLengthStyle)
+	{
+	case FIXED_LENGTH_MESSAGE:
+		break;
+	case VARIABLE_LENGTH_MESSAGE:
+		headerLen += m_iLengthParam;
+		break;
+	default:
+		abort();
+		break;
+	}
+	return headerLen;
+}
+int GecoNetInterfaceElement::NominalBodySize() const
+{
+	return (m_uiLengthStyle == FIXED_LENGTH_MESSAGE) ? m_iLengthParam : 0;
+}
+GecoWatcher* GecoNetInterfaceElementWithStats::GetWatcher()
+{
+	//static FvDirectoryWatcherPtr spWatcher = NULL;
+	//if (spWatcher == NULL)
+	//{
+	//	FvNetInterfaceElementWithStats * pNULL = NULL;
+	//	spWatcher = new FvDirectoryWatcher();
+	//	spWatcher->AddChild("name",
+	//		MakeWatcher(pNULL->m_pcName));
+	//	spWatcher->AddChild("id",
+	//		MakeWatcher(*pNULL, &FvNetInterfaceElementWithStats::IDAsInt));
+	//	spWatcher->AddChild("maxBytesReceived",
+	//		MakeWatcher(pNULL->m_uiMaxBytesReceived));
+	//	spWatcher->AddChild("bytesReceived",
+	//		MakeWatcher(pNULL->m_uiNumBytesReceived));
+	//	spWatcher->AddChild("messagesReceived",
+	//		MakeWatcher(pNULL->m_uiNumMessagesReceived));
+	//	spWatcher->AddChild("avgMessageLength",
+	//		MakeWatcher(*pNULL,
+	//			&FvNetInterfaceElementWithStats::AvgMessageLength));
+	//	spWatcher->AddChild("avgBytesPerSecond",
+	//		MakeWatcher(*pNULL,
+	//			&FvNetInterfaceElementWithStats::AvgBytesReceivedPerSecond));
+	//	spWatcher->AddChild("avgMessagesPerSecond",
+	//		MakeWatcher(*pNULL,
+	//			&FvNetInterfaceElementWithStats::
+	//			AvgMessagesReceivedPerSecond));
+	//}
+	//return spWatcher;
+	return NULL;
+}
--- src/network/net-types.h
+++ src/network/net-types.h
@@ -3,6 +3,7 @@
 #define __GecoNetTypes_H__
 
 #include "common/geco-plateform.h"
+#include "math\ema.h"
 #include "math/geco-math-vector.h"
 #include "math/geco-math-direction.h"
 #include "common/ds/geco-bit-stream.h"
@@ -34,6 +35,16 @@
 #include "common/debugging/spdlog/spdlog.h"
 extern std::shared_ptr<spdlog::logger> g_network_logger;
 
+class GecoWatcher;
+class GecoNetBundle;
+class GecoNetPacket;
+class GecoNetInterfaceElement;
+
+const uchar REPLY_MESSAGE_IDENTIFIER = 0xFF;
+const char FIXED_LENGTH_MESSAGE = 0;
+const char VARIABLE_LENGTH_MESSAGE = 1;
+const char INVALID_MESSAGE = 2;
+const int PACKET_MAX_SIZE = 1472; // 1500 - iphdr 20 - updhdr 8 = 1472
 const int GECO_NET_UDP_OVERHEAD = 28;
 const int GECO_NET_BITS_PER_BYTE = 8;
 const uint GECO_NET_LOCALHOST = 0x0100007F;
@@ -67,7 +78,6 @@ typedef GecoVector3 GecoPosition3D;
 typedef ushort GecoEntityTypeID;
 const GecoEntityTypeID GECO_INVALID_ENTITY_TYPE_ID = GecoEntityTypeID(-1);
 typedef int64 GecoDatabaseID;
-
 typedef uint GecoGridID;
 typedef uint GecoEventNumber;
 const GecoEventNumber GECO_INITIAL_EVENT_NUMBER = 1;
@@ -75,7 +85,6 @@ typedef std::vector< GecoEventNumber > GecoCacheStamps;
 typedef ushort GecoVolatileNumber;
 typedef uchar GecoDetailLevel;
 const int GECO_MAX_DATA_LOD_LEVELS = 6;
-
 typedef int GecoNetSeqNum;
 typedef uchar GecoNetMessageID;
 typedef void * GecoNetTimerID;
@@ -129,17 +138,23 @@ INLINE const char * NetReasonToString(GecoNetReason reason)
 	return "GECO_NET_REASON_UNKNOWN";
 }
 
-class GecoWatcher;
 class GECOAPI GecoNetAddress
 {
+private:
+	static const int MAX_STRLEN = 32;
+	static char ms_pcStringBuf[2][MAX_STRLEN];
+	static int ms_iCurrStringBuf;
+	static char * NextStringBuf();
+
+public:
+	uint m_uiIP;
+	ushort m_uiPort;
+	ushort m_uiSalt;
+
 public:
 	GecoNetAddress();
 	GecoNetAddress(uint ipArg, ushort portArg);
 	GecoNetAddress(char* str);
-
-	uint m_uiIP;
-	ushort m_uiPort;
-	ushort m_uiSalt;
 
 	int WriteToString(char * str, int length) const;
 	operator char*() const { return this->c_str(); }
@@ -150,16 +165,9 @@ public:
 
 	bool IsNone() const { return this->m_uiIP == 0; }
 
-	static GecoWatcher &GetWatcher();
+	static GecoWatcher* GetWatcher();
 
 	static const GecoNetAddress NONE;
-
-private:
-
-	static const int MAX_STRLEN = 32;
-	static char ms_pcStringBuf[2][MAX_STRLEN];
-	static int ms_iCurrStringBuf;
-	static char * NextStringBuf();
 };
 
 INLINE bool operator==(const GecoNetAddress & a, const GecoNetAddress & b);
@@ -202,7 +210,7 @@ INLINE geco_bit_stream_t& operator << (geco_bit_stream_t& kOS, const GecoNetAddr
 GECOAPI bool  WatcherStringToValue(const char *, GecoNetAddress &);
 GECOAPI eastl::string  WatcherValueToString(const GecoNetAddress &);
 
-struct GecoEntityMailBoxRef
+struct GECOAPI GecoEntityMailBoxRef
 {
 	GecoEntityID m_iID;
 	GecoNetAddress m_kAddr;
@@ -271,7 +279,7 @@ INLINE geco_bit_stream_t& operator << (geco_bit_stream_t& kOS, const GecoEntityM
 	return kOS;
 }
 
-class GecoCapabilities
+class GECOAPI GecoCapabilities
 {
 	typedef uint CapsType;
 public:
@@ -287,7 +295,7 @@ private:
 	CapsType m_uiCaps;
 };
 
-class GecoSpaceEntryID : public GecoNetAddress { };
+class GECOAPI GecoSpaceEntryID : public GecoNetAddress { };
 INLINE bool operator==(const GecoSpaceEntryID & a, const GecoSpaceEntryID & b);
 INLINE bool operator!=(const GecoSpaceEntryID & a, const GecoSpaceEntryID & b);
 INLINE bool operator<(const GecoSpaceEntryID & a, const GecoSpaceEntryID & b);
@@ -324,7 +332,7 @@ INLINE geco_bit_stream_t& operator << (geco_bit_stream_t& kOS, const GecoSpaceEn
 	return kOS;
 }
 
-struct InterfaceListenerMsg
+struct GECOAPI InterfaceListenerMsg
 {
 	uint		uiIP;
 	ushort		uiPort;
@@ -379,91 +387,6 @@ INLINE geco_bit_stream_t& operator << (geco_bit_stream_t& kOS, const InterfaceLi
 	return kOS;
 }
 
-INLINE GecoNetAddress::GecoNetAddress()
-{
-}
-
-INLINE GecoNetAddress::GecoNetAddress(uint ipArg, ushort portArg) :
-	m_uiIP(ipArg),
-	m_uiPort(portArg),
-	m_uiSalt(0)
-{
-}
-
-INLINE bool operator==(const GecoNetAddress & a, const GecoNetAddress & b)
-{
-	return (a.m_uiIP == b.m_uiIP) && (a.m_uiPort == b.m_uiPort);
-}
-
-INLINE bool operator!=(const GecoNetAddress & a, const GecoNetAddress & b)
-{
-	return (a.m_uiIP != b.m_uiIP) || (a.m_uiPort != b.m_uiPort);
-}
-
-INLINE bool operator<(const GecoNetAddress & a, const GecoNetAddress & b)
-{
-	return (a.m_uiIP < b.m_uiIP) || (a.m_uiIP == b.m_uiIP && (a.m_uiPort < b.m_uiPort));
-}
-
-INLINE bool operator==(const GecoEntityMailBoxRef & a, const GecoEntityMailBoxRef & b)
-{
-	return (a.m_iID == b.m_iID) && (a.m_kAddr == b.m_kAddr) && (a.m_kAddr.m_uiSalt == b.m_kAddr.m_uiSalt);
-}
-
-INLINE bool operator!=(const GecoEntityMailBoxRef & a, const GecoEntityMailBoxRef & b)
-{
-	return (a.m_iID != b.m_iID) || (a.m_kAddr != b.m_kAddr) || (a.m_kAddr.m_uiSalt != b.m_kAddr.m_uiSalt);
-}
-
-INLINE GecoCapabilities::GecoCapabilities() :
-	m_uiCaps(0)
-{
-}
-
-INLINE void GecoCapabilities::Add(unsigned int cap)
-{
-	m_uiCaps |= 1 << cap;
-}
-
-INLINE bool GecoCapabilities::Has(unsigned int cap) const
-{
-	return !!(m_uiCaps & (1 << cap));
-}
-
-INLINE bool GecoCapabilities::Empty() const
-{
-	return m_uiCaps == 0;
-}
-
-INLINE bool GecoCapabilities::Match(const GecoCapabilities& on,
-	const GecoCapabilities& off) const
-{
-	return (on.m_uiCaps & m_uiCaps) == on.m_uiCaps &&
-		(off.m_uiCaps & ~m_uiCaps) == off.m_uiCaps;
-}
-
-INLINE bool GecoCapabilities::MatchAny(const GecoCapabilities& on,
-	const GecoCapabilities& off) const
-{
-	return !!(on.m_uiCaps & m_uiCaps) && (off.m_uiCaps & ~m_uiCaps) == off.m_uiCaps;
-}
-
-INLINE bool operator==(const GecoSpaceEntryID & a, const GecoSpaceEntryID & b)
-{
-	return operator==((GecoNetAddress)a, (GecoNetAddress)b) && a.m_uiSalt == b.m_uiSalt;
-}
-
-INLINE bool operator!=(const GecoSpaceEntryID & a, const GecoSpaceEntryID & b)
-{
-	return operator!=((GecoNetAddress)a, (GecoNetAddress)b) || a.m_uiSalt != b.m_uiSalt;
-}
-
-INLINE bool operator<(const GecoSpaceEntryID & a, const GecoSpaceEntryID & b)
-{
-	return operator<((GecoNetAddress)a, (GecoNetAddress)b) ||
-		(operator==((GecoNetAddress)a, (GecoNetAddress)b) && (a.m_uiSalt < b.m_uiSalt));
-}
-
 /************************************************************************/
 /*
 弧度=角度乘以π后再除以180
@@ -472,4 +395,245 @@ INLINE bool operator<(const GecoSpaceEntryID & a, const GecoSpaceEntryID & b)
 /************************************************************************/
 typedef GecoVector3 GecoCoord;
 
+struct GECOAPI GecoNetInputMessageHandler
+{
+	virtual ~GecoNetInputMessageHandler() {};
+	virtual void HandleMessage(const GecoNetAddress & source, geco_bit_stream_t & data) = 0;
+};
+
+struct GecoNetPacket
+{
+	typedef ushort Flags;
+	enum
+	{
+		FLAG_HAS_REQUESTS = 0x0001,
+		FLAG_HAS_PIGGYBACKS = 0x0002,
+		FLAG_HAS_ACKS = 0x0004,
+		FLAG_ON_CHANNEL = 0x0008,
+		FLAG_IS_RELIABLE = 0x0010,
+		FLAG_IS_FRAGMENT = 0x0020,
+		FLAG_HAS_SEQUENCE_NUMBER = 0x0040,
+		FLAG_INDEXED_CHANNEL = 0x0080,
+		FLAG_HAS_CHECKSUM = 0x0100,
+		FLAG_CREATE_CHANNEL = 0x0200,
+		KNOWN_FLAGS = 0x03FF
+	};
+
+	typedef ushort Offset;
+	GecoNetPacket* m_spNext;
+	int m_iMsgEndOffset;
+	int m_iFooterSize;
+	int m_iExtraFilterSize;
+	Offset m_uiFirstRequestOffset;
+	Offset *m_puiLastRequestOffset;
+	char m_acData[PACKET_MAX_SIZE];
+
+	char *Back() { return m_acData + m_iMsgEndOffset; }
+	const char *Body() const { return m_acData + 2; } // start of first msg hdr
+
+	GecoNetPacket* Next() { return m_spNext; }
+	const GecoNetPacket* Next() const { return m_spNext; }
+};
+
+class GECOAPI GecoNetInterfaceElement
+{
+public:
+	static const GecoNetInterfaceElement REPLY;
+
+protected:
+	GecoNetMessageID m_uiID;
+	uchar m_uiLengthStyle;
+	int	m_iLengthParam;
+	const char *m_pcName;
+	GecoNetInputMessageHandler *m_pkHandler;
+
+protected:
+	int SpecialExpandLength(void * header, GecoNetPacket * pPacket) const;
+	int SpecialCompressLength(void * header, int length,
+		GecoNetBundle & bundle) const;
+
+public:
+	GecoNetInterfaceElement(const char * name = "", GecoNetMessageID id = 0,
+		uchar lengthStyle = INVALID_MESSAGE, int lengthParam = 0,
+		GecoNetInputMessageHandler * pHandler = NULL) :
+		m_uiID(id),
+		m_uiLengthStyle(lengthStyle),
+		m_iLengthParam(lengthParam),
+		m_pcName(name),
+		m_pkHandler(pHandler)
+	{}
+
+	void Set(const char * name, GecoNetMessageID id, uchar lengthStyle,
+		int lengthParam)
+	{
+		m_uiID = id;
+		m_uiLengthStyle = lengthStyle;
+		m_iLengthParam = lengthParam;
+		m_pcName = name;
+	}
+
+	int HeaderSize() const;
+	int NominalBodySize() const;
+	int CompressLength(void * header, int length, GecoNetBundle & bundle) const;
+	int ExpandLength(void * header, GecoNetPacket * pPacket) const;
+	bool CanHandleLength(int length) const
+	{
+		return (m_uiLengthStyle != VARIABLE_LENGTH_MESSAGE) ||
+			(m_iLengthParam >= int(sizeof(int32))) ||
+			(length < (1 << (8 * m_iLengthParam)) - 1);
+	}
+
+	GecoNetMessageID GetID() const { return m_uiID; }
+	void SetID(GecoNetMessageID id) { m_uiID = id; }
+
+	uchar LengthStyle() const { return m_uiLengthStyle; }
+	int LengthParam() const { return m_iLengthParam; }
+
+	const char *GetName() const { return m_pcName; }
+	const char *c_str() const
+	{
+		static char buf[256];
+		geco_snprintf(buf, sizeof(buf), "%s/%d", m_pcName, m_uiID);
+		return buf;
+	}
+
+	GecoNetInputMessageHandler *GetHandler() const { return m_pkHandler; }
+	void SetHandler(GecoNetInputMessageHandler * pHandler) { m_pkHandler = pHandler; }
+};
+
+class GECOAPI GecoNetInterfaceElementWithStats : public GecoNetInterfaceElement
+{
+private:
+	unsigned int m_uiMaxBytesReceived;
+	unsigned int m_uiNumBytesReceived;
+	unsigned int m_uiNumMessagesReceived;
+	cumulative_ema_t< unsigned int > m_kAvgBytesReceivedPerSecond;
+	cumulative_ema_t< unsigned int > m_kAvgMessagesReceivedPerSecond;
+	static const float AVERAGE_BIAS;
+
+private:
+	int IDAsInt() const { return this->GetID(); }
+
+public:
+	static GecoWatcher* GetWatcher();
+
+public:
+	GecoNetInterfaceElementWithStats() :
+		m_uiMaxBytesReceived(0),
+		m_uiNumBytesReceived(0),
+		m_uiNumMessagesReceived(0),
+		m_kAvgBytesReceivedPerSecond(AVERAGE_BIAS),
+		m_kAvgMessagesReceivedPerSecond(AVERAGE_BIAS)
+	{
+	}
+
+	void MessageReceived(unsigned int msgLen)
+	{
+		++m_uiNumMessagesReceived;
+		++m_kAvgMessagesReceivedPerSecond.value();
+		m_uiNumBytesReceived += msgLen;
+		m_kAvgBytesReceivedPerSecond.value() += msgLen;
+		m_uiMaxBytesReceived = std::max(msgLen, m_uiMaxBytesReceived);
+	}
+
+	void Tick()
+	{
+		m_kAvgBytesReceivedPerSecond.sample();
+		m_kAvgMessagesReceivedPerSecond.sample();
+	}
+
+	unsigned int MaxBytesReceived() const { return m_uiMaxBytesReceived; }
+
+	unsigned int NumBytesReceived() const { return m_uiNumBytesReceived; }
+
+	unsigned int NumMessagesReceived() const { return m_uiNumMessagesReceived; }
+
+	float AvgMessagesReceivedPerSecond() const
+	{
+		return m_kAvgMessagesReceivedPerSecond.average();
+	}
+
+	float AvgBytesReceivedPerSecond() const
+	{
+		return m_kAvgBytesReceivedPerSecond.average();
+	}
+
+	float AvgMessageLength() const
+	{
+		return m_uiNumMessagesReceived ?
+			float(m_uiNumBytesReceived) / float(m_uiNumMessagesReceived) :
+			0.0f;
+	}
+};
+
+
+INLINE GecoNetAddress::GecoNetAddress()
+{
+}
+INLINE GecoNetAddress::GecoNetAddress(uint ipArg, ushort portArg) :
+	m_uiIP(ipArg),
+	m_uiPort(portArg),
+	m_uiSalt(0)
+{
+}
+INLINE bool operator==(const GecoNetAddress & a, const GecoNetAddress & b)
+{
+	return (a.m_uiIP == b.m_uiIP) && (a.m_uiPort == b.m_uiPort);
+}
+INLINE bool operator!=(const GecoNetAddress & a, const GecoNetAddress & b)
+{
+	return (a.m_uiIP != b.m_uiIP) || (a.m_uiPort != b.m_uiPort);
+}
+INLINE bool operator<(const GecoNetAddress & a, const GecoNetAddress & b)
+{
+	return (a.m_uiIP < b.m_uiIP) || (a.m_uiIP == b.m_uiIP && (a.m_uiPort < b.m_uiPort));
+}
+INLINE bool operator==(const GecoEntityMailBoxRef & a, const GecoEntityMailBoxRef & b)
+{
+	return (a.m_iID == b.m_iID) && (a.m_kAddr == b.m_kAddr) && (a.m_kAddr.m_uiSalt == b.m_kAddr.m_uiSalt);
+}
+INLINE bool operator!=(const GecoEntityMailBoxRef & a, const GecoEntityMailBoxRef & b)
+{
+	return (a.m_iID != b.m_iID) || (a.m_kAddr != b.m_kAddr) || (a.m_kAddr.m_uiSalt != b.m_kAddr.m_uiSalt);
+}
+INLINE GecoCapabilities::GecoCapabilities() :
+	m_uiCaps(0)
+{
+}
+INLINE void GecoCapabilities::Add(unsigned int cap)
+{
+	m_uiCaps |= 1 << cap;
+}
+INLINE bool GecoCapabilities::Has(unsigned int cap) const
+{
+	return !!(m_uiCaps & (1 << cap));
+}
+INLINE bool GecoCapabilities::Empty() const
+{
+	return m_uiCaps == 0;
+}
+INLINE bool GecoCapabilities::Match(const GecoCapabilities& on,
+	const GecoCapabilities& off) const
+{
+	return (on.m_uiCaps & m_uiCaps) == on.m_uiCaps &&
+		(off.m_uiCaps & ~m_uiCaps) == off.m_uiCaps;
+}
+INLINE bool GecoCapabilities::MatchAny(const GecoCapabilities& on,
+	const GecoCapabilities& off) const
+{
+	return !!(on.m_uiCaps & m_uiCaps) && (off.m_uiCaps & ~m_uiCaps) == off.m_uiCaps;
+}
+INLINE bool operator==(const GecoSpaceEntryID & a, const GecoSpaceEntryID & b)
+{
+	return operator==((GecoNetAddress)a, (GecoNetAddress)b) && a.m_uiSalt == b.m_uiSalt;
+}
+INLINE bool operator!=(const GecoSpaceEntryID & a, const GecoSpaceEntryID & b)
+{
+	return operator!=((GecoNetAddress)a, (GecoNetAddress)b) || a.m_uiSalt != b.m_uiSalt;
+}
+INLINE bool operator<(const GecoSpaceEntryID & a, const GecoSpaceEntryID & b)
+{
+	return operator<((GecoNetAddress)a, (GecoNetAddress)b) ||
+		(operator==((GecoNetAddress)a, (GecoNetAddress)b) && (a.m_uiSalt < b.m_uiSalt));
+}
 #endif // __GecoNetTypes_H__
